d482a21a9a2fcc826cd307e9d09de02a53b1ae0b
TanishqMotke <tmotke98@gmail.com>
2024-11-13 16:53:39 +0000

    Updated

--- app.py
+++ app.py
@@ -56,7 +56,8 @@ def add():
 
 @app.route("/index.html") #Default - Show Data
 def index(): 
-   return render_template('index.html')
+    return render_template('index.html')
+
 @app.route("/") #Default - Show Data
 def hello(): # Name of the method
   cur = mysql.cursor() #create a connection to the SQL instance
