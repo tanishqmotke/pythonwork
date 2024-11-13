d2fd74d6c7260223b052914ca66f4982ac665343
TanishqMotke <tmotke98@gmail.com>
2024-11-13 16:58:14 +0000

    u

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def add():
 
   return '{"Result":"Success"}'
 
-@app.route("/") #Default - Show Data
+@app.route("/index.html") #Default - Show Data
 def index(): 
     return render_template('index.html')
 
